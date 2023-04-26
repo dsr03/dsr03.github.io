fe2781e6ed7b171c736cd829a7baf339f33fdd36
dsr03 <31076797+dsr03@users.noreply.github.com>
2023-04-26 18:36:31 -0300

    Add files via upload

---
+++ js/index_lda.js
@@ -0,0 +1,203 @@
+$(document).ready(function() {
+	//remover classe de erro
+	function erroClass() {
+		$("form label").removeClass("text-danger");
+	}
+
+	$("form").submit(function(e) {
+		e.preventDefault();
+
+		var a = 0;
+		var b = 0;
+		var c = 0;
+		var d = 0;
+		var e = 0;
+		
+		
+
+		let total_afirmativas = $("form :input[type='radio']").length;
+		let total_respondidas = $("form :input:checked").length;
+
+		//Removendo classe de erro
+		erroClass();
+		//verificando quantidade de respostas
+		if (total_afirmativas / 2 !== total_respondidas) {
+			$(".form-group").each(function(i) {
+				//Recuperar afirmativas não respondidas
+				if (
+					$(this)
+						.find("input")
+						.not(":checked").length > 1
+				) {
+					$(this)
+						.find("label")
+						.addClass("text-danger");
+				}
+			});
+
+			//dando foco na primeira avirmativa não respondida
+			$("form label").each(function(i) {
+				if ($(this).hasClass("text-danger")) {
+					$(this).focus();
+					return false;
+				}
+			});
+		} else {
+            $("form :input:checked").each(function (i) {
+                //quantificando resultado
+				switch ($(this).val()) {
+					case "a":
+						a++;
+						break;
+					case "b":
+						b++;
+						break;
+					case "c":
+						c++;
+						break;
+					case "d":
+						d++;
+						break;
+					case "e":
+						e++;
+						break;
+					default:
+						break;
+				}
+			});
+
+            //Valiando resultado
+			
+            //Resultado no modal
+			$("#res").html("A = " + a + "<br>"+
+						   "B = " + b + "<br>"+
+						   "C = " + c + "<br>"+
+						   "D = " + d + "<br>"+
+						   "E = " + e);
+
+			//Reativando botão de reset
+			$(".btn-secondary").prop("disabled", false);
+            //exibindo modal
+			$("#exampleModal").modal("show");
+		}
+	});
+
+	//Desativar resposta
+	$(document).on("change", ".form-group", function() {
+		$(this)
+			.find("input")
+			.not(":checked")
+			.prop("disabled", true);
+		//removendo erro
+		$(this)
+			.find("label")
+			.removeClass("text-danger");
+	});
+
+	//Reativar opções
+	$(".btn-secondary").click(function() {
+		$("form")
+			.find("input")
+			.not(":checked")
+			.prop("disabled", false);
+		//Removendo mensagens de erro
+		erroClass();
+
+		//Foco na primeira afirmativa
+		$("input[type='radio']")
+			.first()
+			.focus();
+	});
+
+	//Função para iniciar tratamento de javascript
+	function start() {
+		$.getJSON("js/perguntas_lda.json", function(data) {
+			$.each(data, function(i, perguntas) {
+				//inserção da primeira pergunta
+				if (!i) {
+					$("li h5").text("Afirmativa " + (i + 1) + ":");
+					$.each(perguntas, function(j, pergunta) {
+						if (!j) {
+							//Config de Labels
+							$("label")
+                                .attr("for", "r" + i + "_" + j)
+                                .addClass("text-justify")
+								.text(pergunta.resposta);
+							//Config de Input
+							$("input")
+								.attr("id", "r" + i + "_" + j)
+								.attr("name", "r" + i)
+								.val(pergunta.valor);
+							$("div.spinner").remove();
+							$("ul").prop("hidden", false);
+						} else {
+							//clonando estrutura
+							clone = $("div.custom-control").clone();
+							//inserindo dados
+							clone
+								.find("label")
+                                .attr("for", "r" + i + "_" + j)
+                                .addClass("text-justify")
+								.text(pergunta.resposta);
+
+							clone
+								.find("input")
+								.attr("id", "r" + i + "_" + j)
+								.attr("name", "r" + i)
+								.val(pergunta.valor);
+
+							//inserindo no html
+							$("div.form-group").append(clone);
+						}
+					});
+				} else {
+					//recuperar primeira pergunta inserida
+					clone_pergunta = $("li")
+						.first()
+						.clone();
+
+					clone_pergunta.find("h5").text("Afirmativa " + (i + 1) + ":");
+
+					$.each(perguntas, function(j, pergunta) {
+						if (!j) {
+							//recuperando primeira div de pergunta
+							div = clone_pergunta.find("div.custom-control").first();
+
+							//inserindo dados
+							div
+								.find("label")
+                                .attr("for", "r" + i + "_" + j)
+                                .addClass("text-justify")
+								.text(pergunta.resposta);
+
+							div
+								.find("input")
+								.attr("id", "r" + i + "_" + j)
+								.attr("name", "r" + i)
+								.val(pergunta.valor);
+						} else {
+							//recuperando segunda div de pergunta
+							div = clone_pergunta.find("div.custom-control").last();
+							//inserindo dados
+							div
+								.find("label")
+                                .attr("for", "r" + i + "_" + j)
+                                .addClass("text-justify")
+								.text(pergunta.resposta);
+
+							div
+								.find("input")
+								.attr("id", "r" + i + "_" + j)
+								.attr("name", "r" + i)
+								.val(pergunta.valor);
+						}
+					});
+					//inserindo na lista de questões
+					$("ul").append(clone_pergunta);
+				}
+			});
+		});
+	}
+
+	start();
+});
